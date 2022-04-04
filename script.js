b518a039e993ce1f614f29e3e6f1863cddbb30bf
Wilmara Ruiz <wilmara_andreina93@hotmail.com>
2022-04-04 13:36:34 -0500

    Version 2.3

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
-document.addEventListener('DOMContentLoaded',
-function(){
-  obtenerLocalstorage();
-})
+// document.addEventListener('DOMContentLoaded',
+//   function () {
+//     obtenerLocalstorage();
+//   })
 
 const div = document.getElementById("root")
 let boton1 = document.getElementById('botonInicio');
 let boton2 = document.getElementById('verCartas');
 let colors = ["red", "orange", "blue"]
+let i=0;
 
-boton1.addEventListener('click', function(){
+boton1.addEventListener('click', function () {
   alert("Me estan precionando")
-document.getElementById("modal").style.display = "block".display="flex";
+  document.getElementById("modal").style.display = "block".display = "flex";
 })
 
-const url= 'http://hp-api.herokuapp.com/api/characters/';
+const url = 'http://hp-api.herokuapp.com/api/characters/';
 
- fetch(url) //Peticion GET
- .then(res=>res.json())
- .then(data =>{ 
-  
-  localStorage.setItem('personajes',JSON.stringify(data)); 
+fetch(url) //Peticion GET
+  .then(res => res.json())
+  .then(data => {
 
-    data.forEach(personajes => { 
-       
-     const { name, image, house } = personajes;
+    localStorage.setItem('personajes', JSON.stringify(data));
+    data.forEach(personajes => {
 
+      const {
+        name,
+        image,
+        house
+      } = personajes;
 
-     let imagenes=personajes.image
-        
-         num = Math.floor(Math.random() * (3 - 0) + 0)     
-         
+      let nombre = personajes.name;
+      let imagenes=personajes.image;
+ 
+      num = Math.floor(Math.random() * (3 - 0) + 0);
+      boton2.addEventListener('click',(e)=> {
+          e.preventDefault();       
 
-        boton2.addEventListener('click', function(){
-          document.getElementById("modal").style.display = "block".display="none"
-     
-        if(image){
-
+         if (image) {
           
-            div.innerHTML +=`
-             Volver </button>
-        <div class="personajes">
+          document.getElementById("modal").style.display = "block".display = "none";
+          document.getElementById("root").style.display = "block".display = "flex";
+
+         div.innerHTML += `
+          <div class="personajes">
             <img class="personajes" src=${image}><br>
             <div class="texto">
             <p id="nombre" class="nombre">${personajes["name"]}
@@ -47,53 +50,21 @@ const url= 'http://hp-api.herokuapp.com/api/characters/';
             
             </div>  
             </div>  
-            `}
+              `
+        }
+      })
+
+
+    });
+
+  })
+  .catch(err => console.log(err))
+
+function obtenerLocalstorage() {
+  let personajes = localStorage.getItem('personajes');
+  console.log(personajes);
+
+  
+
 }
-)
- 
-   
-   });
 
- })
- .catch( err => console.log(err))
-
- function obtenerLocalstorage()
- {
-     let personajes=localStorage.getItem('personajes');
-     console.log(personajes);
-   
- }
-
-
-
- ///para la funcion recorrer el objeto 
-//  if(direccion1=='atras'){
-//   if(contador==0){
-//     document.getElementById('imagen').src= imagenes[imagenes.length-1];
-    
-//     console(imagenes)
-//     contador++;
-//   }else if (contador<imagenes.length-1) {
-//     document.getElementById('imagen').src= imagenes[imagenes.length-1-contador];
-//     contador++;
-
-//   }else {
-//     document.getElementById('imagen').src= imagenes[0];
-//     contador=0;
-//   }
-// }
-
-// if(direccion1=='adelante'){
-//   if(contador==0){
-//     document.getElementById('imagen').src= imagenes[0];
-//     contador++;
-//   }else if (contador<imagenes.length-1) {
-//     document.getElementById('imagen').src= imagenes[contador];
-//     contador++;
-
-//   }else {
-//     document.getElementById('imagen').src= imagenes[imagenes.length-1];
-//     contador=0;
-//     console.log(imagenes);
-//   }
-// }
